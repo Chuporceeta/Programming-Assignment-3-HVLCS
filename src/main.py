9ac281357b8d084e0f6cae449e35d3a1d27fc596
Xabier <xabier.sienra@gmail.com>
2026-04-02 12:21:49 -0400

    Compute max value

--- src/main.py
+++ src/main.py
@@ -1,11 +1,6 @@
 import os
 import argparse
 
-values = {}
-
-def Val(C):
-    return sum(values[c] for c in C)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, required=True)
@@ -25,21 +20,29 @@ if __name__ == '__main__':
             v = int(v)
             assert v >= 0, "Values must be non-negative"
             values[x] = v
-        A = f.readline()
+        A = f.readline().strip()
         for x in A:
-            assert x in values, ("Strings can only contain characters from "
-                                 "the given alphabet")
-        B = f.readline()
+            assert x in values, "Strings can only contain characters from the given alphabet"
+        B = f.readline().strip()
         for x in B:
-            assert x in values, ("Strings can only contain characters from "
-                                 "the given alphabet")
+            assert x in values, "Strings can only contain characters from the given alphabet"
 
+
+    DP = [[0 for _ in range(len(B)+1)] for _ in range(len(A)+1)]
+    for i in range(1,len(A)+1):
+        for j in range(1,len(B)+1):
+            if A[i-1] == B[j-1]:
+                DP[i][j] = DP[i-1][j-1] + values[A[i-1]]
+            else:
+                DP[i][j] = max(DP[i-1][j], DP[i][j-1])
+
+
+    # BACKTRACKING HERE
     
 
-    output = ''
+    output = str(DP[len(A)][len(B)]) + '\n' # + reconstructed subseq
 
     if output_file is not None:
-        assert os.path.exists(output_file), "Output file does not exist"
         with open(output_file, 'w') as f:
             f.write(output)
     else:
